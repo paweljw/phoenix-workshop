592d918595acc9ff59388f40dcb2763b0a6f92ec
Paweł J. Wal <p@steamshard.net>
2019-05-05 14:15:53 +0200

    Phoenix DB setup

--- example/config/dev.exs
+++ example/config/dev.exs
@@ -6,6 +6,7 @@ config :beeper, Beeper.Repo,
   password: "postgres",
   database: "beeper_dev",
   hostname: "localhost",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
